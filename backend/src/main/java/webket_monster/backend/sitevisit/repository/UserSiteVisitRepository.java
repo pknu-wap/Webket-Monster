a8e7eb14aad67411542f85206ee8a2e1aa0858ab
유수환 <yooz7443@naver.com>
2026-05-29 18:46:41 +0900

    feat: 사이트 방문 repository 조회 기능 추가

--- backend/src/main/java/webket_monster/backend/sitevisit/repository/UserSiteVisitRepository.java
+++ backend/src/main/java/webket_monster/backend/sitevisit/repository/UserSiteVisitRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import webket_monster.backend.sitevisit.domain.UserSiteVisit;
 import webket_monster.backend.user.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserSiteVisitRepository extends JpaRepository<UserSiteVisit, Long> {
 
     Optional<UserSiteVisit> findByUserAndSiteName(User user, String siteName);
+
+    List<UserSiteVisit> findByUser(User user);
 }
