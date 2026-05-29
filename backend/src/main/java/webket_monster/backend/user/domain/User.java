d63cf890a4cdd3a2c96c2cc8c9cc559e4f400009
유수환 <yooz7443@naver.com>
2026-05-29 17:29:49 +0900

    feat: User 아이템 카운트 필드 추가

--- backend/src/main/java/webket_monster/backend/user/domain/User.java
+++ backend/src/main/java/webket_monster/backend/user/domain/User.java
@@ -22,9 +22,41 @@ public class User {
     @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
+    private Integer evolutionStoneCount;
+
+    @Column(nullable = false)
+    private Integer expPotionCount;
+
     @Builder
     public User(String email, String nickname) {
         this.email = email;
         this.nickname = nickname;
+        this.evolutionStoneCount = 0;
+        this.expPotionCount = 0;
+    }
+
+    public void addEvolutionStone(int count) {
+        this.evolutionStoneCount += count;
+    }
+
+    public void useEvolutionStone(int count) {
+        if (this.evolutionStoneCount < count) {
+            throw new IllegalStateException("진화의 돌이 부족합니다.");
+        }
+
+        this.evolutionStoneCount -= count;
+    }
+
+    public void addExpPotion(int count) {
+        this.expPotionCount += count;
+    }
+
+    public void useExpPotion() {
+        if (this.expPotionCount <= 0) {
+            throw new IllegalStateException("경험치 물약이 부족합니다.");
+        }
+
+        this.expPotionCount--;
     }
 }
